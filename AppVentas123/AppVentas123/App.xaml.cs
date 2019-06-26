1be7a0765046c42660631e6bb0c701b837374660
mangovch98 <47693067+mangovch98@users.noreply.github.com>
2019-06-26 18:41:25 -0400

    commit numero 2 con venta123

---
+++ AppVentas123/AppVentas123/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AppVentas.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppVentas.Models;
+using AppVentas.ViewModels;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppVentas
+{
+    public partial class App : Application
+    {
+        public App(String filename)
+        {
+            InitializeComponent();
+
+            VentasViewModel.Inicializador(filename);
+            MainPage = new ListaVentas();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
